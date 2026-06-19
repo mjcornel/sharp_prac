8f44172f8faa9c132d176fdc8161785f05873d88
matthew.j.cornelissen <matthew.j.cornelissen@gmail.com>
2026-06-19 23:37:08 +0200

    Finished part one of the MS collection.

---
+++ LearnBasics/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Lekker");
+Console.WriteLine(" ");
+Console.WriteLine("First one done ez coding env, feel mad dumb.");
+Console.Write(" Testing the write one");
+Console.Write(" Testing it again");
+// Part one done
