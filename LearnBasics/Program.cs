707770117534a7a516521d40abdabca5e3ae4435
matthew.j.cornelissen <matthew.j.cornelissen@gmail.com>
2026-06-20 00:04:16 +0200

    Finished part 2 it was just strings.

--- LearnBasics/Program.cs
+++ LearnBasics/Program.cs
@@ -6,3 +6,20 @@ Console.WriteLine("First one done ez coding env, feel mad dumb.");
 Console.Write(" Testing the write one");
 Console.Write(" Testing it again");
 // Part one done
+//Not ecase sensitive i guess because diff between the '' and ""
+// camel case
+//Theyre doing the variable bit now which is cheese
+//Like i like the practical element but theres really no point in
+//this at this point 
+//Implied type: if the type is var then compiler figures out the
+//actual type based on the value it takes
+//also you have to initialise the vars otherwise compiler will
+//bitch
+var name = "Bob";
+var num = 3;
+var numf = 34.4;
+
+Console.WriteLine("Name is "+ name + "  and i hate "+ num+ " year old");
+Console.Write("using the " + numf);
+
+Console.WriteLine(34.40M);
